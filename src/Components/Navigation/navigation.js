6ff6dbef28f2a43898f08ed6d8d62c56ad945023
supminn <30731236+supminn@users.noreply.github.com>
2021-04-09 18:42:07 +0530

    fix:navlink activeClass

--- src/Components/Navigation/navigation.js
+++ src/Components/Navigation/navigation.js
@@ -15,13 +15,13 @@ export const Navigation = () => {
         <span className="nav-header"> SupMart</span>
       </NavLink>
       <section className="sec-nav-btns">
-        <NavLink to="/products" className="no-line fas fa-lg fa-store secondary-txt" activeClassName="no-line fas fa-lg fa-store primaryBg-txt">
+        <NavLink to="/products" className="no-line fas fa-lg fa-store primaryBg-txt" activeClassName="no-line fas fa-lg fa-store secondary-txt">
         <span className="badge-icon hidden-vis">0</span>
         </NavLink>
-        <NavLink to="/wishlist" activeClassName="no-line fas fa-lg fa-heart primaryBg-txt" className="no-line fas fa-lg fa-heart secondary-txt">
+        <NavLink to="/wishlist" className="no-line fas fa-lg fa-heart primaryBg-txt" activeClassName="no-line fas fa-lg fa-heart secondary-txt">
         <span className={wishes.length>0?"badge-icon primaryBg-txt":"hidden-vis"}>{wishes.length}</span>
         </NavLink>
-        <NavLink to="/cart" className="no-line fas fa-lg fa-shopping-cart secondary-txt" activeClassName="no-line fas fa-lg fa-shopping-cart primaryBg-txt"><span className={totalCartItems>0?"badge-icon primaryBg-txt":"hidden-vis"}>
+        <NavLink to="/cart" activeClassName="no-line fas fa-lg fa-shopping-cart secondary-txt" className="no-line fas fa-lg fa-shopping-cart primaryBg-txt"><span className={totalCartItems>0?"badge-icon primaryBg-txt":"hidden-vis"}>
         {totalCartItems}
       </span></NavLink>        
       </section>
