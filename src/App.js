55dfc94e1734b6357115e853ec8c785bd175a53a
supminn <30731236+supminn@users.noreply.github.com>
2021-05-19 16:27:16 +0530

    fix: footer position

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { useDataContext } from "./Context";
 import {
@@ -15,80 +15,86 @@ import {
   OrderSummary,
   ProductDetails,
   PrivateRoute,
-  Footer
+  Footer,
 } from "./Components";
-import { useAuthContext } from './Context';
-import {useEffect, useMemo} from 'react';
-import axios from 'axios';
-import { updateCart } from './Utils/serverRequests';
-
+import { useAuthContext } from "./Context";
+import { useEffect, useMemo } from "react";
+import axios from "axios";
+import { updateCart } from "./Utils/serverRequests";
 
 function App() {
   const {
-    state: { toastMsg, itemsInCart }, dispatch
+    state: { toastMsg, itemsInCart },
+    dispatch,
   } = useDataContext();
-  const {login, userData, setShowLoader} = useAuthContext();
+  const { login, userData, setShowLoader } = useAuthContext();
 
   const cartItems = useMemo(() => {
-    if(!login && itemsInCart.length>0){
-      return itemsInCart.map(item => item);
+    if (!login && itemsInCart.length > 0) {
+      return itemsInCart.map((item) => item);
     }
-  },[itemsInCart]);
+  }, [itemsInCart]);
 
   useEffect(() => {
-    if(login && userData._id){
-      if(cartItems){
-        cartItems.forEach(product => {
-          while(product.quantity-- > 0){
-            updateCart(
-              product,
-              "ADD",
-              userData._id,
-              dispatch,
-              setShowLoader
-            );
+    if (login && userData._id) {
+      if (cartItems) {
+        cartItems.forEach((product) => {
+          while (product.quantity-- > 0) {
+            updateCart(product, "ADD", userData._id, dispatch, setShowLoader);
           }
-        })
+        });
       }
       (async () => {
-        const {data:{cart}} = await axios.get(`https://api-supminn.herokuapp.com/cart/${userData._id}`);
-          dispatch({type:"SET_CART", payload: cart});
+        const {
+          data: { cart },
+        } = await axios.get(
+          `https://api-supminn.herokuapp.com/cart/${userData._id}`
+        );
+        dispatch({ type: "SET_CART", payload: cart });
       })();
     }
-  },[login, userData])
+  }, [login, userData]);
 
   useEffect(() => {
-    if(login && userData._id){
+    if (login && userData._id) {
       (async () => {
-        const {data:{wishlistItems}} = await axios.get(`https://api-supminn.herokuapp.com/wishlist/${userData._id}`);
-        dispatch({type:"SET_WISHLIST", payload: wishlistItems});
-        const {data:{address}} =  await axios.get(`https://api-supminn.herokuapp.com/address/${userData._id}`);
-        dispatch({type:"SET_ADDRESS", payload: address});
+        const {
+          data: { wishlistItems },
+        } = await axios.get(
+          `https://api-supminn.herokuapp.com/wishlist/${userData._id}`
+        );
+        dispatch({ type: "SET_WISHLIST", payload: wishlistItems });
+        const {
+          data: { address },
+        } = await axios.get(
+          `https://api-supminn.herokuapp.com/address/${userData._id}`
+        );
+        dispatch({ type: "SET_ADDRESS", payload: address });
       })();
+    } else {
+      dispatch({ type: "SET_WISHLIST", payload: [] });
     }
-    else{
-      dispatch({type:"SET_WISHLIST", payload: []});
-    }
-  },[login, userData, dispatch])
-
+  }, [login, userData, dispatch]);
 
   return (
     <div className="App">
       <Navigation />
       <div className="toastmsg-container">{toastMsg && <Toast />}</div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} /> 
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/products/:productId" element={<ProductDetails />} />
-        <PrivateRoute path="/wishlist" element={<Wishlist/>}/>
-        <PrivateRoute path="/address" element={<Address/>}/>
-        <PrivateRoute path="/user-profile" element={<UserProfile/>}/>
-        <PrivateRoute path="/order-summary" element={<OrderSummary/>}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer/>
+      <section className="body-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:productId" element={<ProductDetails />} />
+          <PrivateRoute path="/wishlist" element={<Wishlist />} />
+          <PrivateRoute path="/address" element={<Address />} />
+          <PrivateRoute path="/user-profile" element={<UserProfile />} />
+          <PrivateRoute path="/order-summary" element={<OrderSummary />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </section>
+      <Footer />
     </div>
   );
 }
